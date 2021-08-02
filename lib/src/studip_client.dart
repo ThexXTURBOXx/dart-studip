c696766b55133df522d152da9a2644b354950a98
Nico Mexis <nico.mexis@kabelmail.de>
2021-08-02 19:36:57 +0200

    Add more HTTP possibilities

--- lib/src/studip_client.dart
+++ lib/src/studip_client.dart
@@ -2,6 +2,7 @@ library studip_client;
 
 import 'dart:convert';
 
+import 'package:http/http.dart' as http;
 import 'package:oauth1/oauth1.dart' as oauth1;
 
 /// Provides simple-to-use access to Stud.IP's RestAPI over OAuth 1.
@@ -78,39 +79,155 @@ class StudIPClient {
         _platform.signatureMethod, _clientCredentials, res.credentials);
   }
 
-  /// Returns the body of the given ``url``. This process is async, thus the
-  /// body is returned as a future String instance.
-  Future<String> get(String url) async {
-    return getBody(Uri.parse(url));
-  }
+  /// Returns the body of the given ``url`` after a GET request. This process
+  /// is async, thus the body is returned as a future String instance.
+  Future<String> get(String url, {Map<String, String>? headers}) async =>
+      getBody(Uri.parse(url), headers: headers);
 
-  /// Returns the body of the given ``uri``. This process is async, thus the
-  /// body is returned as a future String instance.
-  Future<String> getBody(Uri uri) async {
-    final res = await client.get(uri);
+  /// Returns the body of the given ``url`` after a POST request. This process
+  /// is async, thus the body is returned as a future String instance.
+  Future<String> post(String url, {Map<String, String>? headers}) async =>
+      postBody(Uri.parse(url), headers: headers);
+
+  /// Returns the body of the given ``url`` after a PUT request. This process
+  /// is async, thus the body is returned as a future String instance.
+  Future<String> put(String url, {Map<String, String>? headers}) async =>
+      putBody(Uri.parse(url), headers: headers);
+
+  /// Returns the body of the given ``url`` after a DELETE request. This process
+  /// is async, thus the body is returned as a future String instance.
+  Future<String> delete(String url, {Map<String, String>? headers}) async =>
+      deleteBody(Uri.parse(url), headers: headers);
+
+  /// Returns the response of the given ``url`` after a GET request. This
+  /// process is async, thus the response is returned as a future response
+  /// instance.
+  Future<http.Response> getResponse(Uri uri,
+          {Map<String, String>? headers}) async =>
+      client.get(uri, headers: headers);
+
+  /// Returns the response of the given ``url`` after a POST request. This
+  /// process is async, thus the response is returned as a future response
+  /// instance.
+  Future<http.Response> postResponse(Uri uri,
+          {Map<String, String>? headers}) async =>
+      client.post(uri, headers: headers);
+
+  /// Returns the response of the given ``url`` after a PUT request. This
+  /// process is async, thus the response is returned as a future response
+  /// instance.
+  Future<http.Response> putResponse(Uri uri,
+          {Map<String, String>? headers}) async =>
+      client.put(uri, headers: headers);
+
+  /// Returns the response of the given ``url`` after a DELETE request. This
+  /// process is async, thus the response is returned as a future response
+  /// instance.
+  Future<http.Response> deleteResponse(Uri uri,
+          {Map<String, String>? headers}) async =>
+      client.delete(uri, headers: headers);
+
+  /// Returns the streamed response after executing the given ``request``.
+  /// This process is async, thus the streamed response is returned as a future
+  /// streamed response instance.
+  Future<http.StreamedResponse> send(http.BaseRequest request) async =>
+      client.send(request);
+
+  /// Returns the body of the given ``uri`` after a GET request. This process
+  /// is async, thus the body is returned as a future String instance.
+  Future<String> getBody(Uri uri, {Map<String, String>? headers}) async {
+    final res = await getResponse(uri, headers: headers);
     if (isErrorCode(res.statusCode)) {
       throw SessionInvalidException(res.statusCode);
     }
     return res.body;
   }
 
-  /// Returns the body of the given ``endpoint`` in the specified RestAPI. To
-  /// proper use, specify ``apiBaseUrl``.
-  Future<String> apiGet(String endpoint) async {
-    return get(apiBaseUrl! + endpoint);
+  /// Returns the body of the given ``uri`` after a POST request. This process
+  /// is async, thus the body is returned as a future String instance.
+  Future<String> postBody(Uri uri, {Map<String, String>? headers}) async {
+    final res = await postResponse(uri, headers: headers);
+    if (isErrorCode(res.statusCode)) {
+      throw SessionInvalidException(res.statusCode);
+    }
+    return res.body;
   }
 
-  /// Returns the body decoded as JSON of the given ``endpoint`` in the
-  /// specified RestAPI. To proper use, specify ``apiBaseUrl``.
-  Future<dynamic> apiGetJson(String endpoint) async {
-    return json.decode(await apiGet(endpoint));
+  /// Returns the body of the given ``uri`` after a PUT request. This process
+  /// is async, thus the body is returned as a future String instance.
+  Future<String> putBody(Uri uri, {Map<String, String>? headers}) async {
+    final res = await putResponse(uri, headers: headers);
+    if (isErrorCode(res.statusCode)) {
+      throw SessionInvalidException(res.statusCode);
+    }
+    return res.body;
   }
+
+  /// Returns the body of the given ``uri`` after a DELETE request. This process
+  /// is async, thus the body is returned as a future String instance.
+  Future<String> deleteBody(Uri uri, {Map<String, String>? headers}) async {
+    final res = await deleteResponse(uri, headers: headers);
+    if (isErrorCode(res.statusCode)) {
+      throw SessionInvalidException(res.statusCode);
+    }
+    return res.body;
+  }
+
+  /// Returns the body of the given ``endpoint`` in the specified RestAPI after
+  /// a GET request. To proper use, specify ``apiBaseUrl``.
+  Future<String> apiGet(String endpoint,
+          {Map<String, String>? headers}) async =>
+      get(apiBaseUrl! + endpoint, headers: headers);
+
+  /// Returns the body of the given ``endpoint`` in the specified RestAPI after
+  /// a POST request. To proper use, specify ``apiBaseUrl``.
+  Future<String> apiPost(String endpoint,
+          {Map<String, String>? headers}) async =>
+      post(apiBaseUrl! + endpoint, headers: headers);
+
+  /// Returns the body of the given ``endpoint`` in the specified RestAPI after
+  /// a PUT request. To proper use, specify ``apiBaseUrl``.
+  Future<String> apiPut(String endpoint,
+          {Map<String, String>? headers}) async =>
+      put(apiBaseUrl! + endpoint, headers: headers);
+
+  /// Returns the body of the given ``endpoint`` in the specified RestAPI after
+  /// a DELETE request. To proper use, specify ``apiBaseUrl``.
+  Future<String> apiDelete(String endpoint,
+          {Map<String, String>? headers}) async =>
+      delete(apiBaseUrl! + endpoint, headers: headers);
+
+  /// Returns the body decoded as JSON of the given ``endpoint`` in the
+  /// specified RestAPI after a GET request. To proper use, specify
+  /// ``apiBaseUrl``.
+  Future<dynamic> apiGetJson(String endpoint,
+          {Map<String, String>? headers}) async =>
+      json.decode(await apiGet(endpoint, headers: headers));
+
+  /// Returns the body decoded as JSON of the given ``endpoint`` in the
+  /// specified RestAPI after a POST request. To proper use, specify
+  /// ``apiBaseUrl``.
+  Future<dynamic> apiPostJson(String endpoint,
+          {Map<String, String>? headers}) async =>
+      json.decode(await apiPost(endpoint, headers: headers));
+
+  /// Returns the body decoded as JSON of the given ``endpoint`` in the
+  /// specified RestAPI after a PUT request. To proper use, specify
+  /// ``apiBaseUrl``.
+  Future<dynamic> apiPutJson(String endpoint,
+          {Map<String, String>? headers}) async =>
+      json.decode(await apiPut(endpoint, headers: headers));
+
+  /// Returns the body decoded as JSON of the given ``endpoint`` in the
+  /// specified RestAPI after a DELETE request. To proper use, specify
+  /// ``apiBaseUrl``.
+  Future<dynamic> apiDeleteJson(String endpoint,
+          {Map<String, String>? headers}) async =>
+      json.decode(await apiDelete(endpoint, headers: headers));
 
   /// Returns whether the given status code should be handled as a error code,
   /// thus indicating, that the session is invalid and should be renewed.
-  bool isErrorCode(int statusCode) {
-    return statusCode == 401 || statusCode == 500;
-  }
+  bool isErrorCode(int statusCode) => statusCode == 401 || statusCode == 500;
 }
 
 /// Thrown when the currently used session is invalid and a new client should be
